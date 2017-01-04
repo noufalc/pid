85115808ba1b04b54b40d2ab4bf589a2489cb7cb
Muhamed Noufal C <mnc22222@gmail.com>
2017-01-04 19:38:13 +0530

    -- cleaned up file. -- need to test

--- pid.py
+++ pid.py
@@ -7,50 +7,33 @@ import os
 
 
 
-class ADRC(object):
+class PID(object):
 
-    '''Defines an ADRC object to implement the speed control of a DC motor.
+    '''Defines an object to implement the speed control of a DC motor.
        No need to pass any arguments'''
 
-    def __init__(self):
+    def __init__(self, K_p, K_i, K_d):
         self.speed = 0
         self.speed_count = 0
         self.interval_start_time = 0
         self.reference_speed = 0
         self.function_time = 0.0 # to be verified
         self.speed_list = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        self.f =  np.array([
-                [   0.95276121411048309006730505643645,    0.007810034411958742713022463277639, 0.000031492074241769127558848023751281],
-                [ -0.093972349537439062161503500192339,     0.99962142058223557583573892770801,   0.0079989868574093583863948708767566],
-                [  -0.06248027529566994864307361012834, -0.00025193659393415296626067556573503,     0.99999932547313674646716208371799]
-                ])      
- 
-        self.h =  np.array([
-                [    0.000031492074241769120782584445716878,  0.04723878588951693768827055919246],
-                [      0.0079989868574093566516713948999495, 0.093972349537439256450532809594733],
-                [ -0.00000067452686319471487005894339006318, 0.062480275295670739676978655552375]
-                ])
-
-
         self.X = np.array([0.0,0.0,0.0])
         self.sampling_time = 8e-3
         self.sampling_frequency = int(1/self.sampling_time)
-        self.K_p = 0.0840  #cl 0.0840 #p  0.0980  #s  0.0462 #n  0.0280 
-        self.K_d = 0.0370  #   0.0370 #   0.0518   #  0.0543 #   0.0329    
-        self.K_i = 0.0476  #   0.0476 #   0.0695   #  0.0262 #   0.0159    
-        self.b = 0.2
+        self.K_p = K_p #cl 0.0840 #p  0.0980  #s  0.0462 #n  0.0280 
+        self.K_i = K_i #   0.0476 #   0.0695   #  0.0262 #   0.0159    
+        self.K_d = K_d #   0.0370 #   0.0518   #  0.0543 #   0.0329    
         self.output_file = open('data.csv', 'w')
         self.pwm_frequency = 20000
-        self.start = 0
+        self.start_time = 0
         self.last_error = 0.0
         self.total_error = 0.0
 
     # lambda function definitions
     current_micro_time = lambda self: int(round(time.time() * 1000000))
-    #f_reference_accelaration = lambda self, t: 240.0*t*((t>0)&(t<=1))+(240.0)*((t>1)&(t<=3))+(-240.0*t+960.0)*((t>3)&(t<=4))+0.0*(t>4)
-    #f_reference_accelaration = lambda self, t: 0.0*t*((t>0)&(t<=4))+(240.0*t - 960.0)*((t>4)&(t<=5))+(240.0)*((t>5)&(t<=7))+(-240.0*t+1920.0)*((t>7)&(t<=8))+0.0*(t>8)
     f_reference_accelaration = lambda self, t: 0.0*t*((t>0)&(t<=4))+(50.0*t - 200.0)*((t>4)&(t<=6))+(100.0)*((t>6)&(t<=12))+(-50.0*t+700.0)*((t>12)&(t<=14))+0.0*(t>14)
-    #f_reference_speed = lambda self, t: 0.0*t*((t>0)&(t<=10))+(600.0)*(t>10)
 
 
     def init_process(self):
@@ -58,10 +41,9 @@ class ADRC(object):
         self.setup_adc()
         self.init_pwm()
         self.setup_gpio()
-        time.sleep(3)
         self.set_interval_start_time()
         self.init_interrupts()
-        self.start = self.current_micro_time()
+        self.start_time = self.current_micro_time()
 
 
     def init_interrupts(self):
@@ -144,34 +126,32 @@ class ADRC(object):
         PWM.cleanup();
 
 
+    def stopProcess(self):
+        self.remove_interrupts()
+        self.output_file.close()
+        PWM.cleanup()
+        print 'Interrupts removed'
+        self.output_file`
+        print 'Process finished'
+
     def calculate(self, args):        
-        t = self.current_micro_time()
         speed = self.get_present_speed();
         reference_speed = self.get_reference_speed() 
         error = reference_speed - speed
         derror = error - self.last_error
-        print error
         self.total_error += error * self.sampling_time
         control_duty = self.K_p * error  + (self.K_d * derror / self.sampling_time) + self.K_i * self.total_error
-        #print "%.5f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t" %(voltage, speed, reference_speed,  X_updated[0], X_updated[1], control_duty),
         self.output_file.write(str(self.function_time) + ' ' +  str(speed) + ' ' + str(reference_speed) + '\n')
-        #self.output_file.write("%.5f\t%.5f\t%.5f\n" %(self.function_time, reference_speed, reference_accelaration))
-        #print ' ', control_duty
         self.last_error = error
         if control_duty > 98.0:
             control_duty = 98.0
         if control_duty < 30.0:
             control_duty = 30.0
         PWM.set_duty_cycle("P9_21", control_duty)
-        #print self.current_micro_time() - t
 
 if __name__ == "__main__":
     os.nice(10)
-    my_adrc = ADRC()
-    my_adrc.init_process()
-    time.sleep(70)
-    my_adrc.remove_interrupts()
-    my_adrc.stopPWM()
-    print 'Interrupts removed'
-    print 'Process finished'
-    PWM.cleanup()
+    my_pid= PID(float(sys.argv[1]),float(sys.argv[2]),float(sys.argv[3]))
+    my_pid.init_process()
+    time.sleep(50)
+    my_pid.stopProcess()
